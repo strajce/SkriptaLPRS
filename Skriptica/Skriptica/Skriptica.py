50b39b43cb4baca902b5b387e2b69f40a6dd17f6
Strahinja Vuckovic <strahinja.vuckovic94@gmail.com>
2017-05-20 12:50:04 +0200

    Finish

--- Skriptica/Skriptica/Skriptica.py
+++ Skriptica/Skriptica/Skriptica.py
@@ -2,7 +2,9 @@ import sys
 import math
 
 T = 2000000
-PI = math.pi 
+PI = math.pi
+
+file = open("Skriptica1.1.txt","w")
 
 for t0 in range(0,1000000,1):
     p=((2*t0)/T-math.sin((4*PI*t0)/T)/(2*PI))*100
@@ -10,6 +12,24 @@ for t0 in range(0,1000000,1):
         if( p>=i+0.9999 and p<=i+1.0001):
             if( p>=i+0.9999 and p<=i+1.00001):
                 if( p>=i+0.99999 and p<=i+1.00001):
-                    print("Procenat je ",i+1,"% , vrednost za P je ",p," , T0 je : ",t0)
+                    s=i+1
+                    file.write("Procenat = %d " %s)
+                    file.write("%")
+                    file.write(", vrednost za p = %0.15f " %p)
+                    file.write(", t0 = %d " %t0)
+                    file.write("\n")
                 else:
-                    print("Procenat je ",i+1,"% , vrednost za P je ",p," , T0 je : ",t0)
+                    s=i+1
+                    file.write("Procenat = %d " %s)
+                    file.write("%")
+                    file.write(", vrednost za p = %0.15f " %p)
+                    file.write(", t0 = %d " %t0)
+                    file.write("\n")
+            else:
+                s=i+1
+                file.write("Procenat = %d " %s)
+                file.write("%")
+                file.write(", vrednost za p = %0.15f " %p)
+                file.write(", t0 = %d " %t0)
+                file.write("\n")
+file.close()
