04543ad46e6a83655f83a0db3e0f0c8a9007daf5
Strahinja Vuckovic <strahinja.vuckovic94@gmail.com>
2017-06-15 11:21:42 +0200

    Optimizovana Skriptica

--- Skriptica/Skriptica/Skriptica.py
+++ Skriptica/Skriptica/Skriptica.py
@@ -4,32 +4,24 @@ import math
 T = 2000000
 PI = math.pi
 
-file = open("Skriptica1.1.txt","w")
+# int -> frac
+fracs = []
+counts = []
+
+for i in range(0,101):
+    fracs.append(1000)
+    counts.append(-1)
+
+file = open("SkripticaHexa.txt","w")
 
 for t0 in range(0,1000000,1):
     p=((2*t0)/T-math.sin((4*PI*t0)/T)/(2*PI))*100
-    for i in range(0,99):
-        if( p>=i+0.9999 and p<=i+1.0001):
-            if( p>=i+0.9999 and p<=i+1.00001):
-                if( p>=i+0.99999 and p<=i+1.00001):
-                    s=i+1
-                    file.write("Procenat = %d " %s)
-                    file.write("%")
-                    file.write(", vrednost za p = %0.15f " %p)
-                    file.write(", t0 = %d " %t0)
-                    file.write("\n")
-                else:
-                    s=i+1
-                    file.write("Procenat = %d " %s)
-                    file.write("%")
-                    file.write(", vrednost za p = %0.15f " %p)
-                    file.write(", t0 = %d " %t0)
-                    file.write("\n")
-            else:
-                s=i+1
-                file.write("Procenat = %d " %s)
-                file.write("%")
-                file.write(", vrednost za p = %0.15f " %p)
-                file.write(", t0 = %d " %t0)
-                file.write("\n")
+    i = round(p)
+    f = abs(p-i)
+    if f<fracs[i]:
+        fracs[i] = f
+        counts[i] = t0
+
+for i in range(0,101):
+    file.write("%d " %i +'=> x"'+'%08x"'%counts[i] + ",\n")
 file.close()
